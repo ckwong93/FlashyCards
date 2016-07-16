d7933fbfd858105f84f3c93821fe76d8219266dc
Jason Sutter <jasonsutter87@gmail.com>
2016-07-16 14:10:03 -0700

    created deck controller

---
+++ app/controllers/deck_controller.rb
@@ -0,0 +1,27 @@
+#display all decks
+get '/decks' do
+end
+
+#display new decks form
+get '/decks/new' do
+end
+
+#create a new decks
+post '/decks' do
+end
+
+#display flashcard by an id
+get '/decks/:id' do
+end
+
+#display a edit flashcard form
+get '/decks/:id/edit' do
+end
+
+#edit a flashcard by and id 
+put '/decks/:id' do
+end
+
+#delete and flashcard by an id
+delete '/decks/:id' do
+end
